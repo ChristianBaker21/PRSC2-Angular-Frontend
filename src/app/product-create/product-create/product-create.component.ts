06a621b9f7d02d8273769929c55ccda71c08ea41
Christian Baker <christianbaker1998@gmail.com>
2020-11-23 16:37:08 -0500

    dropdown work

--- src/app/product-create/product-create/product-create.component.ts
+++ src/app/product-create/product-create/product-create.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Product } from 'src/app/product/product/product.class';
 import { ProductService } from 'src/app/product/product/product.service';
+import { Vendor } from 'src/app/vendor/vendor/vendor.class';
+import { VendorService } from 'src/app/vendor/vendor/vendor.service';
 
 @Component({
   selector: 'app-product-create',
@@ -10,28 +12,52 @@ import { ProductService } from 'src/app/product/product/product.service';
 })
 export class ProductCreateComponent implements OnInit {
 
+vendors: Vendor[]=[]
+  saved: string = "save"
+
   product: Product= new Product();
   constructor(private prodsvc: ProductService,
   private route: ActivatedRoute, 
-  private router: Router
+  private router: Router,
+  private vendserv: VendorService
   ) { }
 
-  save(): void{
-    console.log(this.product);
+  ngOnInit(): void
+    {
+    this.vendserv.list().subscribe(
+      res => {
+        console.log(res);
+        this.vendors = res as Vendor[];
+      },
+      err => 
+      {console.error(err); }
+    )
+    }
+saves(): void
+{
+  this.saved = "Save";
+}
+
+save(): void
+{
+console.log(this.product);
     this.prodsvc.create(this.product).subscribe(
       res => {
         console.debug("Product Create:", res);
         this.router.navigateByUrl("product/list");
       },
-      err => {console.error("Error creating product: ", err); }
-    );
+      err => {console.error("Error creating request: ", err); }
+    )
   }
 
-  ngOnInit(): void {
+
+
    
     
   }
 
-}
+
+
+
 
 
